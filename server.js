7818d8b552622a785ac9abed32662716fbca01b0
Edgar Talledos <onimenotsuki@gmail.com>
2020-09-28 17:55:22 -0500

    Agregado mongo

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const { Wit, log } = require('node-wit');
 const express = require('express');
+const winston = require('winston');
+const chalk = require('chalk');
+const expressWinston = require('express-winston');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const health = require('express-ping');
+const mongoSanitize = require('express-mongo-sanitize');
+const mongoose = require('mongoose');
+const xss = require('xss-clean');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
 const dotenv = require('dotenv');
 
 // Cargamos las rutas
@@ -12,6 +22,9 @@ const googleRoutes = require('./src/routes/google');
 // Providers
 const mailchimp = require('./src/providers/mailchimp');
 const { calendar } = require('./src/providers/google');
+
+// Definimos un separador para el logger
+const separator = '\n========================\n';
 
 // Cargamos las variables de entorno
 dotenv.config();
@@ -36,7 +49,45 @@ const wit = new Wit({
 // Starting our webserver and putting it all together
 const app = express();
 
+// Logger
+app.use(
+  expressWinston.logger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json(),
+      winston.format.simple(),
+    ),
+    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+    msg: `${separator}HTTP:::{{req.method}} {{req.url}}${separator}`,
+    colorize: true,
+    ignoreRoute: () => {
+      return false;
+    },
+  }),
+);
+
+app.use(express.urlencoded({ extended: true }));
+
+// Protección de cabeceras
+app.use(helmet());
+
+// Ping
+app.use(health.ping('/api/v1/ping'));
+
+// CORS
 app.use(cors());
+
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutos
+    max: 2000, // limite el número de solicitudes por IP
+  }),
+);
+
+// Un poco de seguridad
+app.use(mongoSanitize());
+app.use(xss());
 
 app.use(({ method, url }, rsp, next) => {
   rsp.on('finish', () => {
@@ -70,6 +121,9 @@ app.use((req, _, next) => {
   next();
 });
 
+// Ping
+app.use(health.ping('/ping'));
+
 // Shopify routes
 app.use('/shopify', shopifyRoutes);
 
@@ -81,5 +135,32 @@ app.use('/pnl', pnlRoutes);
 
 // Facebook routes
 app.use('/facebook', facebookRoutes);
+
+// Configuración de la base de datos
+mongoose.set('useCreateIndex', true);
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+
+// Nos conectamos a las base de datos en mongo
+mongoose.connect(
+  process.env.NODE_ENV === 'development'
+    ? process.env.MONGO_DB_URI_DEV
+    : process.env.MONGO_DB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
+
+mongoose.connection.on('error', (err) =>
+  console.log(chalk.red('MONGODB::ERROR::'), chalk.red(err)),
+);
+
+mongoose.connection.on('connected', () => {
+  console.log('\n');
+  console.log(chalk.blue('==================== MONGO ===================='));
+  console.log(chalk.blue('MONGO::CONNECT::Conexión hecha con éxito'));
+  console.log(chalk.blue('==================== MONGO ===================='));
+});
 
 app.listen(PORT, () => console.log('Listening on :' + PORT + '...'));
